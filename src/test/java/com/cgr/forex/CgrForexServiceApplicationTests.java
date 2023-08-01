2bce3c673ea7b3327434b84f462ac4ecde16c2c6
USER <USER@Mafer>
2023-08-01 15:11:03 -0500

    Creación del proyecto

---
+++ src/test/java/com/cgr/forex/CgrForexServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgr.forex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgrForexServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
