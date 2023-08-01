3f71cf679d5f1475abde2cfe22357c20987272e5
USER <USER@Mafer>
2023-08-01 18:10:11 -0500

    entidad y controller

---
+++ src/main/java/com/cgr/forex/data/ExchangeValueRepository.java
@@ -0,0 +1,11 @@
+package com.cgr.forex.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+
+	ExchangeValue findByFromAndTo(String from, String to);
+	
+}
